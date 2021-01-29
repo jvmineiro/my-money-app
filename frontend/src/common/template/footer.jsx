675d9749b77b390183e4ea25d9f47ddfd7786de9
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-01-29 14:42:01 -0300

    Rodape colocado

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+  import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2021
+            <a href='http://cod3r.com.br' target='_blank'> JV_Mineiro</a>.
+        </strong>
+    </footer>
+)
