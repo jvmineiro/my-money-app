15e878d541937d9eead29f89621fa9f0f8a99c3b
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-02-16 18:12:18 -0300

    Conectaando ao banco

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -6,6 +6,7 @@ const env = require("../../.env");
 
 const emailRegex = /\S+@\S+\.\S+/;
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/;
+
 const sendErrorsFromDB = (res, dbErrors) => {
   const errors = [];
   _.forIn(dbErrors.errors, (error) => errors.push(error.message));
